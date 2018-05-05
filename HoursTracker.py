60b1c5af78dbb3fb7a8e0ea7c2e20898d5d35848
Denny <denlee973@users.noreply.github.com>
2018-05-05 18:49:37 -0400

    Adding to home, starting new entry
    + Figuring out how to make a listbox with multiple columns
    - Implementing sorting options (but couldn't you just use .sort()?
    + Got an image for the search button but need to resize

--- HoursTracker.py
+++ HoursTracker.py
@@ -23,6 +23,8 @@ class Master:
         f.close
         for i in range(len(self.text)-1):
             self.text[i] = self.text[i][0:len(self.text[i])-1]
+        for j in range(len(self.text)):
+            self.text[j] = self.text[j].split("/t")
         print self.text
         return self.text
             
@@ -58,7 +60,7 @@ class LogIn(Master):
     def checkUserPass(self):
         self.u = self.user.get()
         self.p = self.passw.get()
-        if self.u == self.readFile('data.txt')[0] and self.p == self.readFile('data.txt')[1]:
+        if self.u == self.readFile('data.txt')[0][0] and self.p == self.readFile('data.txt')[1][0]:
             self.window.withdraw()
             self.window = Home(Toplevel(self.window))
             print "You have logged in."
@@ -70,16 +72,44 @@ class Home(Master):
     listEvents = []
     listButtonNames = []
     order = "TitleA"
+    search = ""
     
     def __init__(self,window):
         self.window = window
         window.title("Hours Tracker - Home")
+        window.geometry("1000x1000")
         
+        # search items
         self.search = Entry(window)
-        self.search.grid(row=1, column=2, columnspan=2)
-        # self.searchEnter = Button(window)
+        self.search.grid(row=1, column=1, columnspan=4)
+        self.searchIcon = PhotoImage(file="searchIcon.gif")
+        self.searchEnter = Button(window, image=self.searchIcon)
+        self.searchEnter.grid(row=1, column=5)
+        print "hi"
+        
+        # list of inputted events
+        self.listEvents = self.readFile('data.txt')[2:]
+        print "hi",self.listEvents
+        # self.listEvents.sort()
+        
+        # can you make listbox with different columns?
+        self.listbox = Listbox(window)
+        self.listbox.grid(row=2, column=1)
+        
+        for i in range(len(self.listEvents)):
+            self.listbox.insert(i,self.listEvents[i][0])
+            
+class NewEntry(Master):
+    attributes = ["Subject","With","Date","Time Start","Time End","Other Info"]
+    
+    def __init__(self):
+        self.window = window
+        window.title = "Hours Tracker - New Entry"
+        
+        
+        
+        
 window = Tk()
-
 login = LogIn(window)
 window.mainloop()
     
